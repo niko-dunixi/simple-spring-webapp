eb398a0accc467a1b41144554a5f6ef0f60404d2
Paul Baker <paul.nelson.baker@gmail.com>
2016-12-23 14:44:43 -0700

    Moving the files to the template directory fixes it along with adding content type to the meta tag in the html file.

--- src/main/java/io/paulbaker/friendcode/ProductRepository.java
+++ src/main/java/io/paulbaker/friendcode/ProductRepository.java
@@ -1,10 +1,12 @@
 package io.paulbaker.friendcode;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  * Created by paulbaker on 12/23/16.
  */
+@Repository
 public interface ProductRepository extends PagingAndSortingRepository<Product, Long> {
 
 }
