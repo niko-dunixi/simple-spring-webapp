39b4211708b9ea9e2dc3299fd30efda70c29c239
Paul Baker <paul.nelson.baker@gmail.com>
2016-12-23 13:30:54 -0700

    Automatic browser launch on start.

--- src/main/java/io/paulbaker/friendcode/FriendCodeApplication.java
+++ src/main/java/io/paulbaker/friendcode/FriendCodeApplication.java
@@ -1,14 +1,39 @@
 package io.paulbaker.friendcode;
 
+import org.apache.commons.lang3.SystemUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+import java.awt.*;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.net.URL;
 
 @SpringBootApplication
 @EnableJpaRepositories
 public class FriendCodeApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FriendCodeApplication.class, args);
+    public static void main(String[] args) throws IOException, URISyntaxException {
+        ConfigurableApplicationContext context = SpringApplication.run(FriendCodeApplication.class, args);
+
+        String port = context.getEnvironment().getProperty("local.server.port");
+        String localUrl = "http://localhost:" + port;
+        URL url = new URL(localUrl);
+        if (Desktop.isDesktopSupported()) {
+            Desktop.getDesktop().browse(url.toURI());
+        } else {
+            Runtime runtime = Runtime.getRuntime();
+            if (SystemUtils.IS_OS_WINDOWS) {
+                runtime.exec(new String[]{"start", localUrl});
+            } else if (SystemUtils.IS_OS_MAC) {
+                runtime.exec(new String[]{"open", localUrl});
+            } else if (SystemUtils.IS_OS_LINUX) {
+                runtime.exec(new String[]{"xdg-open", localUrl});
+            } else {
+                System.err.println("Browse to: " + localUrl);
+            }
+        }
     }
 }
